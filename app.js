ede8f875fbfbadc98206e86022e6c8055aee9858
tiffanyluu <tiffanyluu555@gmail.com>
2025-07-18 21:56:45 -0500

    testing render auto deployments

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 
+app.get("/version", (req, res) => {
+  res.send("version 1"); // change this string to ensure a new version deployed
+});
+
 app.listen(PORT, () => {
   //console.log(`server started on port ${PORT}`);
 });
