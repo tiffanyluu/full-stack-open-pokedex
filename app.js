335295bf36be0d73bb76f78181faf288f3ec103e
tiffanyluu <tiffanyluu555@gmail.com>
2025-07-18 21:14:34 -0500

    trying playwright workflow

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 
